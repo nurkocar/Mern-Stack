ac3c1f20ef39b9bf6d20665f4fb7a6b3ffd911db
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-12-13 15:05:44 +0000

    Added BookController

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { check } = require("express-validator");
+const AuthController = require("../controllers/AuthController");
 
 const router = express.Router();
 
-const AuthController = require("../controllers/AuthController");
 // routes for /api/auth
 
 /**
@@ -27,8 +27,19 @@ router.post(
 /**
  * @route   POST /api/auth/login
  * @desc    Login endpoint
- * @access  Private
+ * @access  Public
  */
-router.post("/login", AuthController.authLogin);
+router.post(
+  "/login",
+  [
+    check("email", "Please enter a valid email!").isEmail(),
+    check("password", "Please enter a password with 6 and more chars").isLength(
+      {
+        min: 6,
+      }
+    ),
+  ],
+  AuthController.authLogin
+);
 
-module.exports = router;
+module.exports = router; 
