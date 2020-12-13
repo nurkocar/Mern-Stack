098c0674b1aca676bc07a0d6a1ed8821059b0fe3
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-12-13 16:50:46 +0000

    Created client side

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type = "primary">TEST BUTTON</Button>
+    </div>
+  );
+}
+
+export default App;
